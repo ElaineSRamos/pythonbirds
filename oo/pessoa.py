280bc0fe3581a6193295f65c4721c6e3f55224af
ElaineSRamos <elainesramos@gmail.com>
2018-04-14 18:09:57 -0300

    Criado e removito atributo dinamigo de objetos tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -18,5 +18,11 @@ if __name__ == '__main__':
     for filho in luciano.filhos:
         print(filho.nome)
 
+    luciano.sobrenome = 'Ramalho' #criando um atributo dinamicamente. Nenhum outro
+    #objeto que utilize esta classe será afetada, caso este seja usado.
+    del luciano.filhos #deletando o atributo dinamicamente - nao é uma boa pratica]
+
+    print(luciano.__dict__) #visualizacao da forma  u sera utilizada paar
+    print(renzo.__dict__)
 #    p.nome = 'Elaine'
 #    print( p.nome )
