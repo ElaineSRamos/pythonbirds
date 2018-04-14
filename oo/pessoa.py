8903182270f0943e8aa3cccc831eeee3c281ffbc
ElaineSRamos <elainesramos@gmail.com>
2018-04-14 17:52:09 -0300

    Criada atributos de instancia - nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+    def __init__(self, nome = None, idade = 35):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self): #sempre em um metodo deve-se declarar um atributo
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luiza')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar()) # passagem implicita - quando vc chama o objeto pelo metodo nao é preciso passá-lo como primeiro parmetro
+    print(p.nome)
+    p.nome = 'Elaine'
+    print( p.nome )
