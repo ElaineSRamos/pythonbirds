17de9db2d50235a394984da24bb1358b5dad138e
ElaineSRamos <elainesramos@gmail.com>
2018-04-14 17:44:18 -0300

    Criada metodo cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,9 @@
 class Pessoa:
-    pass
+    def cumprimentar(self): #sempre em um metodo deve-se declarar um atributo
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar()) # passagem implicita - quando vc chama o objeto pelo metodo nao é preciso passá-lo como primeiro parmetro
